9b022d01c0ade4433a88c9563cd7b255e443dac4
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-25 09:25:08 -0600

    Create Hoard and Hoarded tests and methods

--- lib/product.rb
+++ lib/product.rb
@@ -1,14 +1,19 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity
+  attr_reader :category, :name, :unit_price, :quantity, :hoarded
 
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = quantity.to_i 
+    @quantity = quantity.to_i
+    @hoarded = false
   end
 
   def total_price
     @unit_price * @quantity
+  end
+
+  def hoard
+    @hoarded = true 
   end
 end
