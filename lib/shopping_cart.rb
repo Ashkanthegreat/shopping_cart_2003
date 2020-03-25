3c7988fca98c784f3ea654bda88311c477652787
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-25 09:47:30 -0600

    Create add product tests and methods

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,14 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products 
+  attr_reader :name, :capacity, :products
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.chomp('items').to_i
     @products = []
+  end
+
+  def add_product(product)
+    @products << product 
   end
 
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -7,6 +7,8 @@ class ShoppingCartTest < Minitest::Test
 
   def setup
     @cart = ShoppingCart.new("King Soopers", "30items")
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
   end
 
   def test_it_exists
@@ -16,7 +18,13 @@ class ShoppingCartTest < Minitest::Test
   def test_it_has_attributes
     assert_equal 'King Soopers', @cart.name
     assert_equal 30, @cart.capacity
-    assert_equal [], @cart.products 
+    assert_equal [], @cart.products
+  end
+
+  def test_it_can_add_product
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    assert_equal [@product1, @product2], @cart.products 
   end
 
 end
